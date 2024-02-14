c9a3939ba5ff7cc1d91027ab484a77f61a7f771f
vovaent <vovaent@gmail.com>
2024-02-14 17:28:33 +0100

    Removing catalog "build".

--- assets/build/css/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1e41656e042432c7bc02');
--- assets/build/js/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '28cc3348aed498e3cb11');
